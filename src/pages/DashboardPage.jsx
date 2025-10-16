7a1fbc15b55dbb10cc5e2ff66783eb3e82fddb06
cleitonpn <cleitonpnascimento@gmail.com>
2025-10-16 11:51:06 -0300

    Update DashboardPage.jsx

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -978,8 +978,7 @@ const DashboardPage = () => {
               </Button>
             )}
 
-            )}
-
+          
             {userProfile?.funcao === 'gerente' && !isEmpreiteiro && (
               <Button onClick={() => navigate('/relatorios')} variant="ghost" className="w-full justify-start">
                 <BarChart3 className="h-4 w-4 mr-3" />
