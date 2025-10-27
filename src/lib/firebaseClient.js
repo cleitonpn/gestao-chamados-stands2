10813c7f9f5d20673fdebbfebee4c237e5d16cc7
cleitonpn <cleitonpnascimento@gmail.com>
2025-10-27 17:47:57 -0300

    Update firebaseClient.js

--- src/lib/firebaseClient.js
+++ src/lib/firebaseClient.js
@@ -1,6 +1,7 @@
 // src/lib/firebaseClient.js
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getMessaging, isSupported } from "firebase/messaging";
+import { getFirestore } from 'firebase/firestore';
 
 // Se você já tem sua config centralizada, importe daqui:
 import firebaseConfig from "@/config/firebase"; // ajuste o caminho se precisar
@@ -12,6 +13,7 @@ export function getFirebaseApp() {
 export async function getMessagingSafe() {
   const supported = await isSupported().catch(() => false);
   if (!supported) return null;
-  const app = getFirebaseApp();
+  const app = initializeApp(firebaseConfig);
+  export const db = getFirestore(app);
   return getMessaging(app);
 }
